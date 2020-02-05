d800ddb5baf7a4bf49e52c9a37667e02972fbf4d
wareine kana <wareinekana53@gmail.com>
2020-02-05 22:26:33 +0100

    fonctionne jusqu'a la matrice de probabilité

--- tp.c
+++ tp.c
@@ -15,45 +15,32 @@ int main()
 void aff()
 {
     int v, w, i,j;
-    int **T;
-    float **R;//matrice d'adjacence
-    float **P;//matrice de probabilité
-    float **Trans;//matrice transposée
+    int **T,**R;
+    float **Trans,**P;
+    float *tab;
 
-    float *posInit;//position initaile du promeneur
-
-
-    int *tab;
 
     printf("Entrez le nombre de lignes : ");
     scanf("%d", &v);
     printf("Entrez le nombre de colonnes : ");
     scanf("%d", &w);
     T = calloc(v , sizeof(int*));
-    Trans = calloc(v , sizeof(float*));
+	Trans = calloc(v , sizeof(float*));
+	P = calloc(v , sizeof(float*));
 
-    P = calloc(v , sizeof(int*));
+    R = calloc(v-1 , sizeof(int*));
+        tab= calloc(w , sizeof(float));
 
-    tab= calloc(w , sizeof(int));
-	posInit= calloc(w , sizeof(float));
-
-    R = calloc(v-1 , sizeof(float*));
 
     for (i = 0 ; i < v ; i++)
     {
-        T[i] = calloc(w, sizeof(float));
-        Trans[i] = calloc(w, sizeof(float));
+        T[i] = calloc(w, sizeof(int));
+		Trans[i] = calloc(w, sizeof(float));
+		P[i] = calloc(w, sizeof(float));
 
-        P[i] = calloc(w, sizeof(float));
-
-    }
-	
-    for (i = 0 ; i < v-1 ; i++)
-    {
         R[i] = calloc(w, sizeof(int));
 
     }
-	
     printf("Tableau donné à l'initialisation:\n");
     for (i=0; i<v; i++)
     {
@@ -70,47 +57,51 @@ void aff()
   /* Affichage du tableau */
  printf("Tableau donné :\n");
  for (i=0; i<v; i++)
-    {
+   {
      for (j=0; j<w; j++)
+     
          printf("%d", T[i][j]);
+	 
      printf("\n");
-    }
-    /*calcule matrice d'adjancence*/
-	    
+  }
+    printf("cc1");
+     /*calcule matrice d'adjancence*/
 
-    for (i=1; i<v; i++)
-    { printf( "\n i vaut %d",i);
+	for (i=1; i<v; i++)
+    {
      for (j=1; j<w; j++)
      {
 		 if(T[i][j]!=-1)
 		 {
-			R[i-1][T[i][j]]=1;
+			 R[i-1][T[i][j]]=1;
 
-			printf("\n on rempli le tab %d %d avec %.2f \n",i-1,T[i][j],R[i-1][T[i][j]]);
+			printf("\n on rempli le tab %d %d avec %d \n",i-1,T[i][j],R[i-1][T[i][j]]);
 
 		 }
+		 else
+		 {
+			R[i-1][T[i][j]]=0;
+			printf("\n on rempli le tab %d %d avec %d \n",i-1,T[i][j],R[i-1][T[i][j]]);
 
-	  }
-    }
-	
-	for (i=0; i<v; i++)
+		 }
+		 
+	 }
+   }
+	 for (i=0; i<v-1; i++)
     {
      for (j=0; j<w; j++)
-     {
-		 printf("la mat %d %d est %.2f \n",i,j,R[i][j]);
+     //printf("la mat %d %d est %d \n",i,j,R[i][j]);
 
-		 printf("%.2f", R[i][j]);
-	 }
+     printf("%d", R[i][j]);
      printf("\n");
     }
-
-	/*calcul de la transposée*/
+/*calcul de la transposée*/
     printf("matrice transposée \n");
-    for (i=0; i<v; i++)
+    for (i=0; i<v-1; i++)
     {
      for (j=0; j<w; j++)
      {
-		 Trans[i][j]=T[j][i];
+		 Trans[i][j]=R[j][i];
          printf("%.2f", Trans[i][j]);
 	 }
      printf("\n");
@@ -118,10 +109,10 @@ void aff()
     int s=0;
     for(j=0;j<w;j++)
     {
-		for (i=0; i<v; i++)
+		for (i=0; i<v-1; i++)
 		{
 			
-		 s=s+T[j][i];
+		 s=s+Trans[i][j];
 		 tab[j]=s;
 		}
 		 printf("la somme est %d \n",s);
@@ -131,50 +122,30 @@ void aff()
      printf("pour la matrice de probabilité \n");
      for (i=0; i<w; i++)
      {
-        printf("%d", tab[i]);
+        printf("%.2f", tab[i]);
 		printf("\n");
      }
-     printf("matrice de transition ou de probabilité \n");
+    
      int r=0;
 	for (i=0; i<v; i++)
     {
      for (j=0; j<w; j++)
      {
         printf("%.2f", Trans[i][j]/tab[j]);
+        P[i][j]=Trans[i][j]/tab[j];
      }
 		printf("\n");
     }
-    //position aléatoire du promeneur
-    for(i=0;i<w;i++)
-    {
-		posInit[i]=(rand()%2);
-	}
-	printf("la position du promeneur est \n");
+ printf("matrice de transition ou de probabilité \n");
      for (i=0; i<w; i++)
      {
-        printf("%.2f", posInit[i]);
-		printf("\n");
+		 for (j=0; j<w; j++)
+			{
+				printf("%.2f", P[i][j]);
+			}
+				printf("\n");
      }
-
 }
-
-/*calcul de la transposée
-    printf("matrice transposée \n");
-    for (i=0; i<v; i++)
-    {
-     for (j=0; j<w; j++)
-         printf("%d", T[j][i]);
-     printf("\n");
-    }
-     printf("matrice de transition \n");
-	for (i=0; i<v; i+v)
-    {
-     for (j=0; j<w; j++)
-         printf("%d", T[j][i]);
-     printf("\n");
-    }
-*/
-
 
 
 
